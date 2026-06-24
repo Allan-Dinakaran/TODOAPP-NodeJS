ad4a315f3112c439b701257ba547e4b44cbb46e4
Allan Dinakaran <allandinakaran@gmail.com>
2026-06-24 15:41:43 +0530

    port for production deployment

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/api/tasks',task);
 connect_DB();
 
 //Server connection
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
 app.listen(PORT,() => {
   console.log("Server Started");
 });
